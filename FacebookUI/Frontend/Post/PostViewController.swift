11f47717bb2296f6203bfc1516a61f2605437976
takuya <da87435@gmail.com>
2016-10-01 22:49:22 +0900

    first commit

---
+++ FacebookUI/Frontend/Post/PostViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PostViewController.swift
+//  FacebookUI
+//
+//  Created by takuya on 2016/10/01.
+//  Copyright © 2016年 takuya. All rights reserved.
+//
+
+import UIKit
+
+final class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
